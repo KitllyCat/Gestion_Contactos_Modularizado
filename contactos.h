1d79ef12c6e2cb339057e8374265b63b59cde45c
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-30 13:30:31 -0500

    Se añadio el contacto.h, que como cabezera, define y estructura las funciones de contactos.cpp, ademas de ser el conector principal para el main.cpp

---
+++ contactos.h
@@ -0,0 +1,28 @@
+#ifndef CONTACTOS_H
+#define CONTACTOS_H
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+const int tam=99;
+
+struct contactoEmail{
+    string nombres;
+    string sexo;
+    string edad;
+    string telefono;
+    string email;
+    string nacionalidad;
+};
+
+extern contactoEmail conEm[tam];
+extern int cantCont;
+extern int indice;
+
+void agregarContacto();
+void eliminarContacto();
+void mostrarListadoRegistrados();
+void mostrarListadoExistentes();
+
+#endif
